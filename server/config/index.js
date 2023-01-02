c2ad53d035b93eccde2576437b1ece29c9e6b528
NataliiaDyachok <56562370+NataliiaDyachok@users.noreply.github.com>
2023-01-03 00:39:24 +0200

    Revert "the homeworks 15-16 were done"

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: 'server/.env' });
-
-const config = {
-  port: process.env.PORT || 3000,
-};
-
-module.exports = config;
--- server/personages/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var EnumPersonage;
-(function (EnumPersonage) {
-    EnumPersonage["MAGICIAN"] = "m";
-    EnumPersonage["THIEF"] = "t";
-    EnumPersonage["SOLDIER"] = "s";
-})(EnumPersonage || (EnumPersonage = {}));
-exports["default"] = EnumPersonage;
